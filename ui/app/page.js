527bb3c55d8400faaf58d34e8a93a351ca87d6ad
Matt Gunnin <matt@esports.one>
2023-08-04 11:18:29 -0500

    Update agent landing page

--- ui/app/page.js
+++ ui/app/page.js
@@ -1,8 +1,13 @@
+// This code imports the HomeClientPage component from the client-page file.
+// This component contains the landing page for the client.
+// The metadata object contains the title and description of the landing page.
+// The Dashboard component returns the HomeClientPage component.
+
 import HomeClientPage from "./client-page";
 
 export const metadata = {
-  title: "Home | Superagent",
-  description: "Build, deploy and manage AI Agents in seconds",
+  title: "Home | Lacra Labs",
+  description: "Be the future of AI Athletes.",
 };
 
 export default async function Dashboard() {
